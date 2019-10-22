cb1480f1ff2b81ae93211b2fc76dee69c82d5c60
liuyongshunu <spx-liuys@supaur.com>
2019-10-22 19:08:06 +0800

    fsdf

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -1,11 +1,21 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-console.log(__dirname,  '33333',path.join(__dirname), 'fffkffffkfkfk')
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+console.log(__dirname, '33333', path.join(__dirname), 'fffkffffkfkfk')
+console.log(path.resolve(__dirname, '../src/api'), 'kkkkkkkkkkkkkkkkkkkkkk')
 module.exports = {
   entry: path.join(__dirname, '../src/index.tsx'),
   output: {
     filename: 'bundle.js',
     path: path.join(__dirname, '../dist')
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json'],
+    alias: {
+      '@': path.resolve(__dirname, '../src'),
+      '@api': path.resolve(__dirname, '../src/api'),
+      '@router': path.resolve(__dirname, '../router')
+    }
   },
   module: {
     rules: [
@@ -15,30 +25,43 @@ module.exports = {
         include: path.join(__dirname, '../src')
       },
       {
-        test: /\.(j|t)sx?$/,
-        use: ['babel-loader'],
-        include: [path.resolve('../src')],
-        // 排除node_modules底下的
-        exclude: /node_modules/
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader'
       }
+      // {
+      //   test: /\.js$/,
+      //   enforce: 'pre',
+      //   loader: 'source-map-loader'
+      // },
+      // {
+      //   test: /\.(j|t)sx?$/,
+      //   use: ['babel-loader'],
+      //   include: [path.resolve('../src')],
+      //   // 排除node_modules底下的
+      //   exclude: /node_modules/
+      // }
     ]
   },
+  externals: {
+    react: 'React',
+    'react-dom': 'ReactDOM'
+  },
   plugins: [
-      new HtmlWebpackPlugin({
-        filename: 'index.html',
-        template: 'public/index.html',
-        inject: true
-      })
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'public/index.html',
+      inject: true
+    })
   ],
-   devServer: {
-      host: 'localhost',
-      port: 8077,
-      historyApiFallback: true,
-      overlay: {
-        //当出现编译器错误或警告时，就在网页上显示一层黑色的背景层和错误信息
-        errors: true
-      },
-      inline: true,
-      hot: true
-    }
+  devServer: {
+    host: 'localhost',
+    port: 8077,
+    historyApiFallback: true,
+    overlay: {
+      //当出现编译器错误或警告时，就在网页上显示一层黑色的背景层和错误信息
+      errors: true
+    },
+    inline: true,
+    hot: true
+  }
 }
