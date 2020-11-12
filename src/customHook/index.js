462f9f7633d317c34ac8b1f33184c7ba31edad21
xiaozhai <liuyongshun@126.com>
2020-11-12 21:16:24 +0800

    f

--- src/customHook/index.js
+++ src/customHook/index.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 
-function useFriendStatus (friendID) {
+function useFriendStatus(friendID) {
   const [isOnline, setIsOnline] = useState(null)
 
   useEffect(() => {
